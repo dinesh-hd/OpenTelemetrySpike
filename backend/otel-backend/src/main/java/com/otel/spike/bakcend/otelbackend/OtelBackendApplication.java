041cf4367ef3d4b5a64a4dc2324aeb2cfb638550
Dinesh <dinesh.hema99@gmail.com>
2023-11-11 13:02:35 +0000

    Spring boot Backend Skeleton Setup

---
+++ backend/otel-backend/src/main/java/com/otel/spike/bakcend/otelbackend/OtelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.otel.spike.bakcend.otelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelBackendApplication.class, args);
+	}
+
+}
---
+++ backend/otel-backend/src/test/java/com/otel/spike/bakcend/otelbackend/OtelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otel.spike.bakcend.otelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
