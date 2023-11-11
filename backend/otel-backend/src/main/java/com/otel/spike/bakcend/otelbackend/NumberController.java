766d1d9c4e60c10e74aa85e052bc53cfc8f470c0
Dinesh <dinesh.hema99@gmail.com>
2023-11-11 13:14:12 +0000

    Added a simple increment end point to the backend

---
+++ backend/otel-backend/src/main/java/com/otel/spike/bakcend/otelbackend/NumberController.java
@@ -0,0 +1,20 @@
+package com.otel.spike.bakcend.otelbackend;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NumberController {
+
+    @GetMapping("/increment")
+    @ResponseBody
+    public String getIncerementOf(@RequestParam int number){
+        
+        int incremement = number + 1;
+
+        return Integer.toString(incremement);
+    }
+}
